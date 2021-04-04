518b61e78ace9d20f90cd90a57b01b366d901abb
Moh Amilin <amilinmoh99@gmail.com>
2021-04-04 13:31:48 +0700

    update : re-using components

---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+export default function Todos() {
+  return (
+    <>
+      <div className="card">
+        <h2>Learn React</h2>
+        <div className="actions">
+        <button className="btn">Edit</button>
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </>
+  );
+}
