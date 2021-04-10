e5620d12fe2bbc0421007eddb9f7fb1e06f6b237
Moh Amilin <amilinmoh99@gmail.com>
2021-04-10 22:08:47 +0700

    update : create new state for modal

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,21 @@
+import React, {useState} from 'react'
+import Backdrop from './Backdrop'
+import Modal from './Modal'
+
 export default function Todos(props) {
+  const [modalOpen, setModalOpen] = useState(false)
+  console.log('modalOpen', modalOpen)
+  console.log('setModalOpen', setModalOpen)
+
   // untuk mengecek apakah value dalam props sudh ada atau belum
   console.log('pros', props)
 
   function deleteHandler(){
-    // untuk mengecek apakah btn delete sudah sesuai dngan value props nya.
-    console.log('klik deletes', props.text)
+    setModalOpen(true)
+  }
+
+  function cancelModal(){
+    setModalOpen(false)
   }
 
   return (
@@ -15,6 +26,8 @@ export default function Todos(props) {
         <button className="btn">Edit</button>
           <button className="btn" onClick={deleteHandler} >Delete</button>
         </div>
+        {modalOpen && <Modal onCancel={cancelModal} onConfrim={cancelModal} />}
+        {modalOpen && <Backdrop onCancel={cancelModal} />}
       </div>
     </>
   );
